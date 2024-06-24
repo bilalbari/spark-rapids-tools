b11910656de7967f35a7d83d93ce39e44b5f01a8
Sayed Bilal Bari <sbari@nvidia.com>
2024-06-24 14:03:21 -0500

    Benchmarking using spark benchmark classes
    Signed-off-by: Sayed Bilal Bari <sbari@nvidia.com>

--- core/src/main/scala/com/nvidia/spark/rapids/tool/benchmarker/BenchmarkerSpark.scala
+++ core/src/main/scala/com/nvidia/spark/rapids/tool/benchmarker/BenchmarkerSpark.scala
@@ -5,21 +5,29 @@ import scala.concurrent.duration.DurationInt
 import com.nvidia.spark.rapids.tool.qualification.QualificationArgs
 import com.nvidia.spark.rapids.tool.qualification.QualificationMain.mainInternal
 
-import org.apache.spark.sql.rapids.tool.Benchmark
+import org.apache.spark.sql.rapids.tool.{Benchmark, BenchmarkBase}
 
-object BenchmarkerSpark {
-  def main(args: Array[String]): Unit = {
-    val benchmarker = new Benchmark("QualificationBenchmark", 2, warmupTime = 50.seconds)
-    benchmarker.addCase("QualificationBenchmark") { _ =>
-      val qualificationArgs = Array("--output-directory",
-        "/home/sbari/project-repos/scratch_folder/issue-367/output_folder",
-        "--per-sql","true",
-        "/home/sbari/project-repos/scratch_folder/issue-978/eventlogs/temp-event-logs/databricks",
-      )
-      mainInternal(new QualificationArgs(qualificationArgs),
-        printStdout = true, enablePB = true)
+object BenchmarkerSpark extends BenchmarkBase{
+
+  def test(): Unit = {
+    runBenchmark("QualificationBenchmark"){
+      val benchmarker = new Benchmark("QualificationBenchmark", 2,
+        warmupTime = 50.seconds, output = output)
+      benchmarker.addCase("QualificationBenchmark") { _ =>
+        val qualificationArgs = Array("--output-directory",
+          "/home/sbari/project-repos/scratch_folder/issue-367/output_folder",
+          "--per-sql","true",
+          "/home/sbari/project-repos/scratch_folder/issue-978/eventlogs/temp-event-logs/databricks",
+        )
+        mainInternal(new QualificationArgs(qualificationArgs),
+          printStdout = true, enablePB = true)
+      }
+
+      benchmarker.run()
     }
+  }
 
-    benchmarker.run()
+  override def runBenchmarkSuite(mainArgs: Array[String]): Unit = {
+    test()
   }
 }
